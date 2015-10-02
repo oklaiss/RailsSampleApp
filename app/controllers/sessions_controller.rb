0ed5e570a3cbb3da7571aeab56e041426e1f2099
Owen Klaiss <oklaiss@smu.edu>
2015-10-01 21:37:56 -0500

    Finished user editing, updating, indexing and destroying paths

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,16 +4,14 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-      if user && user.authenticate(params[:session][:password])
-        # Log the user in and redirect to the user's show page.
-        log_in user
-        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-        redirect_to user
-      else
-        # Create an error message.
-        flash.now[:danger] = 'Invalid email/password combination'
-        render 'new'
-      end
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_back_or user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
   end
 
   def destroy
