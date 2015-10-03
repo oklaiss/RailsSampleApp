8c16720f5618b0212cb3a22e6f1ef5133a5ab472
Owen Klaiss <oklaiss@smu.edu>
2015-10-03 18:21:51 -0500

    test changes

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,9 +3,9 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => 'AKIAIQ4XFLWSCTEGSIGQ',
-      :aws_secret_access_key => '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'
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory     =  'owenrails'
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
